c83272cda0fec530c4e3d9befe5ad7859f976c98
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-29 17:31:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -135,3 +135,25 @@ class Ptero(pygame.sprite.Sprite):
             self.current_image = 0
         self.image = self.sprites[int(self.current_image)]
 
+
+
+# Variables
+
+
+game_speed = 5
+jump_count = 10
+player_score = 0
+game_over = False
+obstacle_timer = 0
+obstacle_spawn = False
+obstacle_cooldown = 1000
+
+# Surfaces
+
+ground = pygame.image.load("assets/ground.png")
+ground = pygame.transform.scale(ground, (1280, 20))
+ground_x = 0
+ground_rect = ground.get_rect(center=(640, 400))
+cloud = pygame.image.load("assets/cloud.png")
+cloud = pygame.transform.scale(cloud, (200, 80))
+
