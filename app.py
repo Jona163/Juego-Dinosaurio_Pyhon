c17ebb6d228ab66ce1cc2de3d4a7cc61dfdcb4a6
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-29 17:30:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,28 @@
 # Fecha: 29 Septiembre 2024
 # Descripción: Código game-dino.
 # GitHub: https://github.com/Jona163
+
+import pygame
+import sys
+import random
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Dino Game")
+
+game_font = pygame.font.Font("assets/PressStart2P-Regular.ttf", 24)
+
+# Classes
+
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self, image, x_pos, y_pos):
+        super().__init__()
+        self.image = image
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        self.rect.x -= 1
