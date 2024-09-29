e6176e7c1050f1187bcac1d13fb83f409be77b03
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-29 17:30:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,36 @@ class Cloud(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x -= 1
+
+
+
+class Dino(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.running_sprites = []
+        self.ducking_sprites = []
+
+        self.running_sprites.append(pygame.transform.scale(
+            pygame.image.load("assets/Dino1.png"), (80, 100)))
+        self.running_sprites.append(pygame.transform.scale(
+            pygame.image.load("assets/Dino2.png"), (80, 100)))
+
+        self.ducking_sprites.append(pygame.transform.scale(
+            pygame.image.load(f"assets/DinoDucking1.png"), (110, 60)))
+        self.ducking_sprites.append(pygame.transform.scale(
+            pygame.image.load(f"assets/DinoDucking2.png"), (110, 60)))
+
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.current_image = 0
+        self.image = self.running_sprites[self.current_image]
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.velocity = 50
+        self.gravity = 4.5
+        self.ducking = False
+
+    def jump(self):
+        jump_sfx.play()
+        if self.rect.centery >= 360:
+            while self.rect.centery - self.velocity > 40:
+                self.rect.centery -= 1
