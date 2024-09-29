e78b25c7673328be661004bf412979eb9ff27678
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-29 17:30:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -60,3 +60,48 @@ class Dino(pygame.sprite.Sprite):
         if self.rect.centery >= 360:
             while self.rect.centery - self.velocity > 40:
                 self.rect.centery -= 1
+
+
+    def duck(self):
+        self.ducking = True
+        self.rect.centery = 380
+
+    def unduck(self):
+        self.ducking = False
+        self.rect.centery = 360
+
+    def apply_gravity(self):
+        if self.rect.centery <= 360:
+            self.rect.centery += self.gravity
+
+    def update(self):
+        self.animate()
+        self.apply_gravity()
+
+    def animate(self):
+        self.current_image += 0.05
+        if self.current_image >= 2:
+            self.current_image = 0
+
+        if self.ducking:
+            self.image = self.ducking_sprites[int(self.current_image)]
+        else:
+            self.image = self.running_sprites[int(self.current_image)]
+
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.sprites = []
+        for i in range(1, 7):
+            current_sprite = pygame.transform.scale(
+                pygame.image.load(f"assets/cacti/cactus{i}.png"), (100, 100))
+            self.sprites.append(current_sprite)
+        self.image = random.choice(self.sprites)
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        self.x_pos -= game_speed
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
